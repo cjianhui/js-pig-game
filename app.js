a20a54be0097df46e3610fb381a0510cbf466068
cjianhui <cjianhui1993@gmail.com>
2019-01-24 17:15:33 +0800

    Added button event listener and set all scores to 0

--- app.js
+++ app.js
@@ -8,3 +8,32 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+scores = [0, 0];
+roundScore = 0;
+activePlayer = 0;
+
+
+document.querySelector('.dice').style.display = 'none';
+
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+document.querySelector('.btn-roll').addEventListener('click', function() {
+
+    // 1. Get random number
+    var dice = Math.floor(Math.random() * 6) + 1;
+
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update round score if rolled number was not a 1
+
+});
